d4516d8191a47bbe828ba087f31a898c4e04ea98
Azis Zuhri Pratomo <aziszuhrip354@gmail.com>
2023-07-01 12:00:33 +0700

    update: project

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function createUser(Request $request) {
+        $name = $request->username;
+        $email = $request->email;
+        $password = $request->password;
+        $typeUserRole = 1;
+        $newUser = new User();
+        $newUser->name = $name;
+        $newUser->email = $email;
+        $newUser->password = $password;
+        $newUser->user_type_role = $typeUserRole;
+        $newUser->save();
+
+        return \response()->json([
+            "data" => $newUser,
+            "message" => "Berhasil buat user baru",
+            "status" => true,
+        ]);
+    }
 }
